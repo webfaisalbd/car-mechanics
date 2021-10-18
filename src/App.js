f42d24d9f4f865eb81a87eceea79b3c9791759c4
unknown <web.faisal.bd@gmail.com>
2021-10-18 12:12:04 +0600

    banner carousel done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Experts from './Pages/Home/Experts/Experts';
+import Home from './Pages/Home/Home/Home';
 import Services from './Pages/Home/Services/Services';
 
 function App() {
   return (
     <div className="App">
-      <Services></Services>
-      <Experts></Experts>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Experts from '../Experts/Experts';
+import Services from '../Services/Services';
+
+
+const Home = () => {
+    return (
+        <div>
+
+            <Banner></Banner>
+            <Services></Services>
+            <Experts></Experts>
+                
+        </div>
+    );
+};
+
+export default Home;
